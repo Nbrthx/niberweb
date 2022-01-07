c69433960f2da76382910b0c4f09ca3e3b948603
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-01-07 21:05:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const router = express.Router()
-
-router.get('/',function(req,res){
-  res.sendFile('index.html');
-  //__dirname : It will resolve to your project folder.
-});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-//add the router
-app.use('/', router);
+app.get('/',function(req,res){
+  res.sendFile('index');
+  //__dirname : It will resolve to your project folder.
+});
 
 app.listen(1274, () => {
   console.log('Listening at 1274')
