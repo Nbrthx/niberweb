b406e98ccdb5c4ea64d71f1280e97cd450db825b
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-01-07 21:33:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path = require('path')
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',function(req,res){
-  res.sendFile(__dirname+'/public/index.html');
+app.get('/',(req,res) => {
+  res.sendFile(__dirname+'/index.html');
   //__dirname : It will resolve to your project folder.
 });
 
