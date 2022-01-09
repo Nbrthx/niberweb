be1538c53cde8bc339a6d52cdf219956c8b80f47
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-01-09 23:18:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/',(req,res) => {
-  res.sendFile(__dirname+'/public/index.html');
+  res.sendFile(__dirname+'/index.html');
   //__dirname : It will resolve to your project folder.
 });
 
