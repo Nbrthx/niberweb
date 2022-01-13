4d415637187b7dbb66ee6991b8cf7979baf3548b
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-01-14 00:19:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { Pool, Client } = require('pg')
 
 const dburl = process.env.DATABASE_URL
 
-const pg = Pool(
+const pg = new Pool(
   connectionString: dburl
 });
 console.log(pg.query("SELECT count FROM counter"))
