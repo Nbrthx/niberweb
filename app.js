96c4e2c1dfdc02f598e4b8e3f3db88a41fc8d27c
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-01-13 17:35:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = process.env.PORT || 3000
+const { Pool } = require('pg')
+const pool = new Pool({
+  user: secrets.PGUSER,
+  host: secrets.PGHOST,
+  database: secrets.PGDB,
+  password: secrets.PGPASS,
+  port: secrets.PGPORT
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res) 
+  pool.end() 
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 
