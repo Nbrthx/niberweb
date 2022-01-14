4c45d37d5715379fe0fa5dc578629c266da612ca
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-01-14 14:24:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ pg.query('SELECT count FROM counter WHERE id=1', (err, res) => {
 
 app.get('/',(req,res) => {
   count++;
-  res.render('index',{count});
+  res.render('index',{ count: count });
   count--;
   pg.query('UPDATE counter SET count=count+1 where id=1', (err, res) => {
     pg.end() 
