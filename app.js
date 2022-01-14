e8bc597df88c1cdeda28f75e2a9288156ec141d7
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-01-14 22:37:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ app.set('view engine', 'ejs')
 app.get('/', (req,res) => {
   pool.query('UPDATE counter SET count=count+1 where id=1')
 
-  try {
-    const row = await pool.query("SELECT count FROM counter WHERE id=1")
-    count = row.rows[0].count
-  } catch (err) {
-    console.log(err.stack)
-  }
+  pool.query("SELECT count FROM counter WHERE id=1")
+    .then(row => count = row.rows[0].count)
+    .catch(err => setImmediate(() => {
+        throw err
+      })
+    )
   
   res.render('index',{ count: count });
   
