249169dd32d84040f4cc1d18d35a69f3049c70a2
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-01-30 22:39:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -17,12 +17,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   pool.query('UPDATE counter SET count=count+1 where id=1')
 
-  pool.query("SELECT * FROM counter WHERE id=1", (row, err) => {
-    count = row
-    console.log(err)
+  pool.query("SELECT count FROM counter WHERE id=1", (err, row) => {
+    count = row.rows[0].count
     res.render('index',{ count: count })
   })
 });
