ac98ea8559ce45178de952e30ed7d1905b9bfbc7
nbrthx@proton.me <nbrthx@proton.me>
2023-03-29 12:49:47 +0700

    update

--- src/Component/Index.svelte
+++ src/Component/Index.svelte
@@ -14,34 +14,94 @@
     })
 </script>
 
-<div id="header">
-        <label class="head-text">Niberthix</label>
+  <div class="header">
+        <span class="head-text">Niberthix</span>
       </div>
-      <div id="content">
-        <div id="box-head">
-          <div class="cent" id="pp"></div>
+      <div class="content">
+        <div class="box-head">
+          <img class="cent" id="pp" src="https://thumbs2.imgbox.com/70/67/TmP11ARk_t.png" loading="eager">
           <h1 class="c3f9 cent">Hello World</h1>
           <p class="cent">
-            This is portofolio of<br>
+            This is IT portofolio of<br>
           Ektada.
           <p>
         </div>
         <hr>
-        <div id="box">
-          <h2>Paragraph 1</h2>
-          In here, i'm just bored and type some word at this website space, nothing in here.
+        <div class="box">
+          <h2>Hi!</h2>
+          My name is Ektada, and Niberthix is my username on the internet. I'm Programmer and Designer from Indonesia. I started learning Programmer and Designer from 12 years old or 2019. I'm an expert on javascript language and node js runtime and svelte framework.
         </div>
-        <div id="box">
-          <h2>Paragraph 2</h2>
-          This web design has created by Spck editor on android. Yea, i am created this website with android.
+        <div class="box">
+          <h2>My Experience</h2>
+          <div class="card">
+            <iframe
+src="https://youtube.com/embed/NA4XvV3uTY4">
+</iframe>
+            <h3>Create a Game</h3>
+            <p>I try to make 2D game with godot engine. Full assets of that used on video created by myself.</p>
+          </div>
+          <div class="card">
+            <iframe
+src="https://youtube.com/embed/_J7HDoxADis">
+</iframe>
+            <h3>Speedrun Create a Game</h3>
+            <p>I challenge myself to make 2D platformer game with godot engine in 10 minutes. Full assets of that used on video created by myself too.</p>
+          </div>
+          <div class="card">
+            <img class="imgc" src="https://images2.imgbox.com/f5/24/x8V2SROZ_o.jpg" load="lazy">
+            <h3>Create Simple Chat App</h3>
+            <p>Chat app that created with socket io. I dont have documentation of it(screenshot is old design). But i have the code of it.</p>
+            <a href="https://github.com/Nbrthx/dapp-chat"><button class="btn">Github Chatapp</button></a>
+          </div>
+          <div class="card">
+            <img class="imgc" src="https://images2.imgbox.com/c5/24/h5Z2Ghe2_o.png" load="lazy">
+            <h3>Create a Minecraft Server and Plugin</h3>
+            <p>Minecraft server with theme of MMORPG. I compile plugin myself on Eclipse and then I named it "Raveling". This is where I start learning programming Java</p>
+            <a href="https://github.com/Nbrthx/raveling"><button class="btn">Github Raveling</button></a>
+            <a href="https://www.spigotmc.org/resources/raveling-mmorpg-plugin.94929/"><button class="btn">Spigot Raveling</button></a>
+          </div>
+          <div class="card">
+            <img class="imgc" src="https://images2.imgbox.com/6f/e8/IFZJF7Ti_o.png" load="lazy">
+            <h3>Create a Little Bot Discord</h3>
+            <p>Bot discord that can auto reply a messeges. Available rules agrements with emotes and connect to my plugin, so i can do a comment from discord. But i dont have code of it.</p>
+          </div>
+          <div class="card">
+            <img class="imgc" src="https://images2.imgbox.com/0b/e0/ZjDPf02M_o.png" load="lazy">
+            <h3>Create Database Admin for PostgreSQL</h3>
+            <p>I just want to manage my postgre database quickly on web. Because nothing or i can't found web app like that, so i made it myself. But, this UI is still look bad.</p>
+            <a href="https://github.com/Nbrthx/pogemin"><button class="btn">Github Pogemin</button></a>
+          </div>
         </div>
-        <div id="box">
-          <h2>Paragraph 3</h2>
-          Want to see a button design, look down.<br>
-          <input type="button" class="btn" value="Nothing in here">
+        <div class="box">
+          <h2>I Can Do</h2>
+          <div class="box-list">
+            <h3>Make web design</h3>
+          </div>
+          <div class="box-list">
+            <h3>Make web system or backend</h3>
+          </div>
+          <div class="box-list">
+            <h3>Make a little game</h3>
+          </div>
+          <div class="box-list">
+            <h3>Manage SQL database</h3>
+          </div>
+          <div class="box-list">
+            <h3>Make simple illustrations or icon</h3>
+          </div>
+        </div>
+        <div class="box">
+          <h2>Closing</h2>
+          <img class="imgo" src="https://thumbs2.imgbox.com/e8/7b/CCd1rJHs_t.jpg" load="lazy">
+          <p>
+            I'm so sad because I have to end all project and my inspiration. I should go to boarding school and i can't play with my PC again. I hope I have laptop in my boarding school to continue my project or start new project. but I am limited by my finances.
+          </p><br/>
+          <p>So, if you want donate me, you can contact <a href="https://wa.me/62895377298126">My Brother</a> or <a href="https://wa.me/6287739786776">My Father</a> to do it. Or if you want to be my friend, dm <a href="https://instagram.com/nbrthx">Me On Instagram</a>.</p><br/>
+          <b>Thanks For Visiting</b>
+          
         </div>
       </div>
-      <div id="end">
+      <div class="end">
         <h2 class="c3f9">Contact</h2>
         Youtube: <a href="https://youtube.com/@nbrthx">Niberthix</a><br>
         Github: <a href="https://github.com/Nbrthx">Nbrthx</a>
